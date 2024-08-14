36fbae79465daf5adccbac5d4e6bf765dd0a1ace
matveyvarlamov <matvejvarlamov495@gmail.com>
2024-08-15 00:31:57 +0700

    fixed bug

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-s = input()
-
 def valid(s):
     stack = []
     for char in s:
         if char in '([{':
             stack.append(char)
-        elif char == ')' and stack[-1] == '(':
+        elif char == ')' and stack and stack[-1] == '(':
             stack.pop()
-        elif char == ']' and stack[-1] == '[':
+        elif char == ']' and stack and stack[-1] == '[':
             stack.pop()
-        elif char == '}' and stack[-1] == '{':
+        elif char == '}' and stack and stack[-1] == '{':
             stack.pop()
-    if len(stack) == 0:
-        return True
-    return False
+        else:
+            return False
+    return len(stack) == 0
 
+
+s = input("Введите строку со скобками: ")
 res = valid(s)
 print(res)
